04c7de49ad5ffa4fac94e4a17adf3980c2009a39
jangjh0201 <jangjh0201@gmail.com>
2024-07-10 15:13:40 +0900

    데이터베이스 설정 변경

--- database/database.py
+++ database/database.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:system@localhost:3306/ice_cream_db"
+DATABASE_URL = "mysql+pymysql://root:system@localhost:3306/ice_cream_db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
