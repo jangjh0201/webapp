22916d5fea97a00f1de8e94d844cb96ca23f7500
jangjh0201 <jangjh0201@gmail.com>
2024-07-18 17:49:46 +0900

    컨트롤러 템플릿, 키오스크 반환 추가

--- app/controller/controller.py
+++ app/controller/controller.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI, Request, Form, Depends, HTTPException
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
+from starlette.responses import JSONResponse
 
 # 프로젝트 루트 디렉토리를 sys.path에 추가
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
@@ -34,7 +35,16 @@ def show_home(request: Request):
 
 @app.get("/item")
 def show_item(request: Request, db: Session = Depends(get_db)):
-    return item_service.show_item(request, db, templates)
+    ice_creams, toppings, consumables = item_service.show_item(db)
+    return templates.TemplateResponse(
+        "item.html",
+        {
+            "request": request,
+            "ice_creams": ice_creams,
+            "toppings": toppings,
+            "consumables": consumables,
+        },
+    )
 
 
 @app.post("/item")
@@ -46,19 +56,31 @@ def add_item(
     item_quantity: int = Form(...),
     db: Session = Depends(get_db),
 ):
-    return item_service.add_item(
-        request, item_type, item_name, item_price, item_quantity, db, templates
-    )
+    item_service.add_item(item_type, item_name, item_price, item_quantity, db)
+    return show_item(request, db)
 
 
 @app.delete("/item/{item_type}/{item_id}")
 def remove_item(item_type: str, item_id: int, db: Session = Depends(get_db)):
-    return item_service.remove_item(item_type, item_id, db)
+
+    if item_service.remove_item(item_type, item_id, db):
+        return {"success": True}
+    else:
+        return {"success": False}
 
 
 @app.get("/order")
 def show_order(request: Request, db: Session = Depends(get_db)):
-    return order_service.show_order(request, db, templates)
+    ice_creams, toppings, consumables = order_service.show_order(db)
+    return templates.TemplateResponse(
+        "order.html",
+        {
+            "request": request,
+            "ice_creams": ice_creams,
+            "toppings": toppings,
+            "consumables": consumables,
+        },
+    )
 
 
 @app.post("/order")
@@ -69,21 +91,68 @@ def add_order(
     consumable_ids: str = Form(None),
     db: Session = Depends(get_db),
 ):
-    return order_service.add_order(
-        request, ice_cream_id, topping_ids, consumable_ids, db, templates
-    )
+    order_service.add_order(ice_cream_id, topping_ids, consumable_ids, db)
+    return templates.TemplateResponse("index.html", {"request": request})
 
 
 @app.post("/kiosk")
 async def add_order_by_kiosk(request: Request, db: Session = Depends(get_db)):
-    return await order_service.add_order_by_kiosk(request, db)
+    body = await request.json()
+    order_details = body.get("OR", {})
+
+    try:
+        result = await order_service.add_order_by_kiosk(order_details, db)
+        return JSONResponse(status_code=201, content={"OR": result})
+    except HTTPException as e:
+        details = e.detail
+        error_messages = []
+        for detail in details:
+            if detail["code"] == 404:
+                if detail["type"] == "icecream":
+                    error_messages.append(
+                        f"아이스크림 '{detail['name']}'가 존재하지 않습니다."
+                    )
+                elif detail["type"] == "topping":
+                    error_messages.append(
+                        f"토핑 '{detail['name']}'가 존재하지 않습니다."
+                    )
+            elif detail["code"] == 409:
+                if detail["type"] == "icecream":
+                    error_messages.append(
+                        f"아이스크림 '{detail['name']}'의 재고가 부족합니다."
+                    )
+                elif detail["type"] == "topping":
+                    error_messages.append(
+                        f"토핑 '{detail['name']}'의 재고가 부족합니다."
+                    )
+                elif detail["type"] == "consumable":
+                    error_messages.append(
+                        f"소모품 '{detail['name']}'의 재고가 부족합니다."
+                    )
+        return JSONResponse(
+            status_code=e.status_code, content={"OR": {"detail": error_messages}}
+        )
 
 
 @app.get("/stock")
 def show_inventory(request: Request, db: Session = Depends(get_db)):
-    return item_service.show_inventory(request, db, templates)
+    ice_creams, toppings, consumables = item_service.show_inventory(db)
+    return templates.TemplateResponse(
+        "stock.html",
+        {
+            "request": request,
+            "inventory_data": {
+                "ice_cream": ice_creams,
+                "topping": toppings,
+                "consumable": consumables,
+            },
+        },
+    )
 
 
 @app.get("/history")
 def show_history(request: Request, db: Session = Depends(get_db)):
-    return order_service.show_history(request, db, templates)
+    orders = order_service.show_history(db)
+    return templates.TemplateResponse(
+        "history.html", {"request": request, "orders": orders}
+    )
