d18afef59025dc1c77fc722b2a55c0ef560b0e42
jangjh0201 <jangjh0201@gmail.com>
2024-07-19 18:14:47 +0900

    테스트 데이터 수정

--- app/main.py
+++ app/main.py
@@ -41,4 +41,4 @@ add_test_data(db)
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="info")
