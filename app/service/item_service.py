f730fd658c8b1946d8022baeea30a5f2a960ed0f
jangjh0201 <jangjh0201@gmail.com>
2024-07-19 14:50:25 +0900

    item service 변경

--- app/service/item_service.py
+++ app/service/item_service.py
@@ -1,44 +1,19 @@
-from fastapi import Request, Depends
 from sqlalchemy.orm import Session
-from fastapi.templating import Jinja2Templates
-from models.models import Consumable, IceCream, Topping
-from database.crud.ice_cream import (
-    create_ice_cream,
-    read_all_ice_creams,
-)
-from database.crud.topping import (
-    create_topping,
-    read_all_toppings,
-)
-from database.crud.consumable import (
-    create_consumable,
-    read_all_consumables,
-)
+from models.models import IceCream, Topping, Consumable
+from database.crud.ice_cream import create_ice_cream, read_all_ice_creams
+from database.crud.topping import create_topping, read_all_toppings
+from database.crud.consumable import create_consumable, read_all_consumables
 
 
-def show_item(request: Request, db: Session, templates: Jinja2Templates):
+def show_item(db: Session):
     ice_creams = read_all_ice_creams(db)
     toppings = read_all_toppings(db)
     consumables = read_all_consumables(db)
-    return templates.TemplateResponse(
-        "item.html",
-        {
-            "request": request,
-            "ice_creams": ice_creams,
-            "toppings": toppings,
-            "consumables": consumables,
-        },
-    )
+    return ice_creams, toppings, consumables
 
 
 def add_item(
-    request: Request,
-    item_type: str,
-    item_name: str,
-    item_price: int,
-    item_quantity: int,
-    db: Session,
-    templates: Jinja2Templates,
+    item_type: str, item_name: str, item_price: int, item_quantity: int, db: Session
 ):
     if item_type == "ice_cream":
         create_ice_cream(db, item_name, item_price, item_quantity)
@@ -46,7 +21,7 @@ def add_item(
         create_topping(db, item_name, item_price, item_quantity)
     elif item_type == "consumable":
         create_consumable(db, item_name, item_price, item_quantity)
-    return show_item(request, db, templates)
+    return True
 
 
 def remove_item(item_type: str, item_id: int, db: Session):
@@ -57,18 +32,11 @@ def remove_item(item_type: str, item_id: int, db: Session):
     elif item_type == "consumable":
         db.query(Consumable).filter(Consumable.id == item_id).delete()
     db.commit()
-    return {"success": True}
+    return True
 
 
-def show_inventory(request: Request, db: Session, templates: Jinja2Templates):
-    ice_creams = read_all_ice_creams(db)
-    toppings = read_all_toppings(db)
-    consumables = read_all_consumables(db)
-    inventory_data = {
-        "ice_cream": {ic.name: ic.quantity for ic in ice_creams},
-        "topping": {tp.name: tp.quantity for tp in toppings},
-        "consumable": {cs.name: cs.quantity for cs in consumables},
-    }
-    return templates.TemplateResponse(
-        "stock.html", {"request": request, "inventory_data": inventory_data}
-    )
+def show_inventory(db: Session):
+    ice_creams = {ic.name: ic.quantity for ic in read_all_ice_creams(db)}
+    toppings = {tp.name: tp.quantity for tp in read_all_toppings(db)}
+    consumables = {cs.name: cs.quantity for cs in read_all_consumables(db)}
+    return ice_creams, toppings, consumables
