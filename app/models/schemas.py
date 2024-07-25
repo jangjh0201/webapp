d4c0af0b66119a5b107691d9481440615f50fa7a
jangjh0201 <jangjh0201@gmail.com>
2024-07-25 18:02:42 +0900

    User 스키마, 목업DB 생성

--- app/models/schemas.py
+++ app/models/schemas.py
@@ -48,3 +48,16 @@ class RobotLog(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class User(BaseModel):
+    username: str
+    password: str
+
+
+FAKE_USERS_DB = {
+    "testuser": {
+        "username": "testuser",
+        "password": "test",  # 실제로는 해시된 비밀번호를 사용해야 합니다.
+    }
+}
