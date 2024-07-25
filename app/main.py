1130ac482e8adbc2bff8bfe4916f7437757296e3
jangjh0201 <jangjh0201@gmail.com>
2024-07-25 17:58:38 +0900

    컨트롤러 분할(auth, user, manager)

--- app/main.py
+++ app/main.py
@@ -1,16 +1,31 @@
 import sys
 import os
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from sqlalchemy.orm import Session
 
 # 프로젝트 루트 디렉토리를 sys.path에 추가
 sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
-from controller.controller import app
+from controller.user_controller import router as user_router
+from controller.manager_controller import router as manager_router
+from controller.auth_controller import router as auth_router  # 새로 추가된 라우터
 from database.database import initialize_tables, SessionLocal
 from database.crud.ice_cream import create_ice_cream
 from database.crud.topping import create_topping
 from database.crud.consumable import create_consumable
+
+app = FastAPI()
+
+# 정적 파일 설정
+app.mount("/static", StaticFiles(directory="app/resource/static"), name="static")
+
+# 유저, 매니저 및 인증 라우터 등록
+app.include_router(user_router)
+app.include_router(
+    manager_router
+)  # 매니저 라우터 prefix="/manager"로 변경 시 다른 컨트롤러 수정 필요
+app.include_router(auth_router)  # 새로 추가된 라우터
 
 # 테이블 및 데이터베이스 설정
 initialize_tables()
@@ -21,17 +36,12 @@ db = SessionLocal()
 
 # 테스트용 데이터 추가
 def add_test_data(db: Session):
-    # 아이스크림 생성
     create_ice_cream(db, "mint", 2500, 100)
     create_ice_cream(db, "choco", 2500, 100)
     create_ice_cream(db, "strawberry", 2500, 100)
-
-    # 토핑 생성
     create_topping(db, "chocoball", 500, 100)
     create_topping(db, "cereal", 700, 100)
     create_topping(db, "oreo", 700, 100)
-
-    # 소모품 생성
     create_consumable(db, "cup", 0, 200)
 
 
